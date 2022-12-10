cec5724ca1b0670b197d8f68e2a84cc2d0a1884c
Daniel Celestino <danielcelestino@gmail.com>
2022-12-10 10:07:31 -0300

    Adding code

---
+++ src/main/java/br/com/daniel/ApiGatewayCambioApplication.java
@@ -0,0 +1,13 @@
+package br.com.daniel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayCambioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayCambioApplication.class, args);
+	}
+
+}
